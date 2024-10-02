a86d4f83445b308e844d518ef899a60a9a6db5e6
atalaykut <atalaykutcloud@gmail.com>
2024-10-02 21:10:24 +0200

    Favoriten Bilder und Aykut Fragment added

---
+++ app/src/main/java/com/example/myrealmeal/ui/AykutFragment.kt
@@ -0,0 +1,29 @@
+package com.example.myrealmeal.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myrealmeal.R
+import com.example.myrealmeal.databinding.FragmentAykutBinding
+
+
+class AykutFragment : Fragment() {
+
+    private lateinit var binding: FragmentAykutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentAykutBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
--- app/src/main/java/com/example/myrealmeal/ui/RecipeFragment.kt
+++ app/src/main/java/com/example/myrealmeal/ui/RecipeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.myrealmeal.R
 import com.example.myrealmeal.databinding.FragmentRecipeBinding
 import com.example.myrealmeal.databinding.FragmentRecipeFavoriteBinding
@@ -24,6 +25,18 @@ class RecipeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //
+        binding.btnAykutFragment.setOnClickListener {
+
+            findNavController().navigate(R.id.aykutFragment)
+        }
+
+        binding.btnRecipeDetails.setOnClickListener {
+
+            findNavController().navigate(R.id.action_recipeFragment_to_recipeDetailFragment)
+
+        }
     }
 
 }
